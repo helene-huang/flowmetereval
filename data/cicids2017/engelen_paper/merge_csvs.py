019fa4530718b787c5cb7279c3acd063cfb8c12c
thomasmarchioro3 <thomasmarchioro3@gmail.com>
2026-09-12 17:50:35 +0200

    update download script and README in cicids2017/engelen_paper

--- data/cicids2017/engelen_paper/merge_csvs.py
+++ data/cicids2017/engelen_paper/merge_csvs.py
@@ -2,12 +2,13 @@ from pathlib import Path
 
 CURR_DIR: Path = Path(__file__).resolve().parent
 filenames: list[str] = ["monday.csv", "tuesday.csv", "wednesday.csv", "thursday.csv", "friday.csv"]
-filepaths: list[Path] = [CURR_DIR / filename for filename in filenames]
+CSV_DIR: Path = CURR_DIR / "csvs"
+filepaths: list[Path] = [CSV_DIR / filename for filename in filenames]
 output_filepath: Path = CURR_DIR / "cicids2017.csv"
 
 def main() -> None:
     """
-    NOTE: This script assumes all csv files are in the root directory and have the same header
+    NOTE: This script assumes all csv files are in the 'csvs' subdirectory and have the same header
     """
     with open(output_filepath, "w") as output_file:
         for i, filepath in enumerate(filepaths):
